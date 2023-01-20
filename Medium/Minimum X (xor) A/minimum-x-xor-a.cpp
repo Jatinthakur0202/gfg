23b7567b105d9e5adcaf5f6bc4509a12521c1e46
Jatin <71177163+Jatinthakur0202@users.noreply.github.com>
2023-01-21 01:14:15 +0530

    Updated Solution - GfG to GitHub

--- Medium/Minimum X (xor) A/minimum-x-xor-a.cpp
+++ Medium/Minimum X (xor) A/minimum-x-xor-a.cpp
@@ -10,23 +10,28 @@ using namespace std;
 class Solution {
   public:
     int minVal(int a, int b) {
- int c = __builtin_popcount(b), d = 0, i = (1 << 30);
-        while(i && c) {
-            if(a & i) {
-                d += i;
-                c -= 1;
+        // code here
+   int setBits = __builtin_popcount(b);
+        int setBits1 = __builtin_popcount(a);
+        int ans = 0;
+
+        for (int i = 0; i <= 31; i++) {
+            int mask = 1 << i;
+            int set = a & mask;
+
+            // If i'th bit is set also set the
+            // same bit in the required number
+            if (set == 0 && setBits > setBits1) {
+                ans |= (mask);
+                setBits--;
+            } else if (set && setBits1 > setBits) {
+                setBits1--;
+            } else {
+                ans |= set;
             }
-            i >>= 1;
         }
-        i = 1;
-        while(i && c) {
-            if(!(a & i)) {
-                d += i;
-                c -= 1;
-            }
-            i <<= 1;
-        }
-        return d;
+
+        return ans;
     }
 };
 
