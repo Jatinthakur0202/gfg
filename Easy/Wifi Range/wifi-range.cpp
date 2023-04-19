f7ca6104ec6c444ea35b514b23d1ea2d616afd08
Jatin <71177163+Jatinthakur0202@users.noreply.github.com>
2023-04-19 11:33:16 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Wifi Range/wifi-range.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    bool wifiRange(int N, string S, int X){
+        // code here
+          int i = 0, wifi = 0;
+        
+        while(i < N && wifi >= -X)  S[i++]-'0' ? wifi = X : wifi-- ;
+            
+        return i==N && wifi>=0;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        int N, X;
+        string S;
+        cin >> N >> X >> S;
+        Solution obj;
+        cout << obj.wifiRange(N, S, X) << "\n";
+    }
+}
+
+// } Driver Code Ends
